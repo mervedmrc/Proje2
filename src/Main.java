92b66b626c2d2a6aee4928e2186c2854faf5e897
mervedmrc <samanyolu.700@hotmail.com>
2014-07-08 10:22:03 +0300

    third commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by xmdemirci on 08.07.2014.
+ */
+public class Main {
+
+    public static void main(String[]args ) {
+
+        System.out.println("merhaba dünya");
+
+    }
+
+
+
+}
